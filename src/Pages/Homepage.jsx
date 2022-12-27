90dbe71d02b27f33a9c3a9382cf048825137fbbc
testuser <testuser@gmail.com>
2022-12-27 22:00:24 +0530

    created input and fetch data from maza api

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Mainpagelayout from '../Componenets/Mainpagelayout';
 
-
 const Homepage = () => {
-  return (
-    <Mainpagelayout> home page it is</Mainpagelayout>
-     
-    
-  )
-}
+  const [input, setInput] = useState('');
 
-export default Homepage
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/people?q=${input}`)
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = e => {
+    if (e.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <Mainpagelayout>
+      <input
+        type="text"
+        onChange={e => setInput(e.target.value)}
+        value={input}
+        onKeyDown={onKeyDown}
+      />
+      <button type="button" onClick={onSearch}>
+        search
+      </button>
+    </Mainpagelayout>
+  );
+};
+
+export default Homepage;
