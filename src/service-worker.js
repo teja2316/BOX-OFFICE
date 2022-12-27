5f258f2190bc9c184883ff1b12a03f2b17c95fdf
testuser <testuser@gmail.com>
2022-12-27 18:33:10 +0530

    initiased and installed dev dependencies

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,6 +7,8 @@
 // You can also remove this file if you'd prefer not to use a
 // service worker, and the Workbox build step will be skipped.
 
+/* eslint-disable */
+
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
