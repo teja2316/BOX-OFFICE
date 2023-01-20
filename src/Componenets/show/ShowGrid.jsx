0797806d3773fc7ae5f9aebe042154b31bc9e0d0
testuser <testuser@gmail.com>
2023-01-20 19:15:03 +0530

    using customhooks for starred shows

--- src/Componenets/show/ShowGrid.jsx
+++ src/Componenets/show/ShowGrid.jsx
@@ -2,19 +2,38 @@ import React from 'react';
 import ShowCard from './ShowCard';
 import Image_not_found from '../../images/Image_not_found.jpg';
 import { FlexGrid } from '../styled';
+import { useShows } from '../../misc/custom-hooks';
 
 const ShowGrid = ({ data }) => {
+
+  const [starredShows, dispatchStarred] = useShows();
+
   return (
     <FlexGrid>
-      {data.map(({ show }) => (
+      {data.map(({ show }) => {
+
+        const isStarred = starredShows.includes(show.id)
+
+        const onStarClick = () =>{
+
+          if(isStarred){
+            dispatchStarred({type: "REMOVE", showId: show.id })
+          }else{
+            dispatchStarred({type: "ADD", showId: show.id })
+          }
+        }
+        return(          
         <ShowCard
           key={show.id}
           id={show.id}
           name={show.name}
           image={show.image ? show.image.medium : Image_not_found}
           summary={show.summary}
-        />
-      ))}
+          onStarClick={onStarClick}
+          isStarred={isStarred}
+        />      
+        )
+      })}
     </FlexGrid>
   );
 };
