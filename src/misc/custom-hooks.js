7a20b49a031d9f24954308f83834b8a571c036e1
testuser <testuser@gmail.com>
2023-01-20 18:48:12 +0530

    created customhook for shows

---
+++ src/misc/custom-hooks.js
@@ -0,0 +1,34 @@
+import { useEffect, useReducer } from 'react';
+
+function showsReducer(previousState, action){
+    switch(action.type){
+        case "ADD": {
+            return [...previousState, action.showId];
+        }
+
+        case "REMOVE": {
+            return previousState.filter(showId => showId !== action.showId);
+        }
+        default: return previousState;
+    }
+}
+
+
+function usePersistedReducer(reducer, initialState, key) {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persisted = localStorage.getItem(key);
+
+    return persisted ? JSON.parse(persisted) : initial;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+
+  return [state, dispatch];
+
+};
+
+export function useShows(key='shows'){
+    return usePersistedReducer(showsReducer, [], key);
+}
